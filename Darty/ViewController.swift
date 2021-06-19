96723c47f18058ca5fdb8206287065d13718f53a
Ru5C55an <ruslan.sn@icloud.com>
2021-06-19 19:17:26 +0300

    Create project

---
+++ Darty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Darty
+//
+//  Created by Руслан Садыков on 19.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
